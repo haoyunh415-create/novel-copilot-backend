6cb1f8739283d096e5029343d29a483f1edaaaa7
haoyunh415-create <haoyunh415@gmail.com>
2026-07-02 16:51:28 +0800

    扩展默认地址改为云服务器 8.134.8.50:8000

--- JianLai_Helper/popup.js
+++ JianLai_Helper/popup.js
@@ -5,7 +5,7 @@ let isStarting = false;
 function getAPI() {
   return new Promise((resolve) => {
     chrome.storage.local.get(["api_url"], ({ api_url }) => {
-      resolve(api_url || "http://127.0.0.1:8000");
+      resolve(api_url || "http://8.134.8.50:8000");
     });
   });
 }
